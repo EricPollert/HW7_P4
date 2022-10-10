8e6117035f357756357085895a8ca402dc8c41d8
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-10 00:04:08 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW7P4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+  int num_days;
+  cin >> num_days;
+  int junk[num_days];
+  for(int n = 0; n < num_days; n++)
+    {
+      cin >> 
+
+      
+    }
+    
+}
